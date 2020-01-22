3331db519e18080bb3379367d2af29747308cf6c
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-22 12:06:10 -0800

    getting into a habbit of commiting

---
+++ data/product.js
@@ -0,0 +1,74 @@
+const noodle = {
+    id: 'pool noodle',
+    name: 'pool noodle',
+    description: 'Not sure what they are for, but they float';
+    image: 'https://storage.needpix.com/rsynced_images/pool-noodle-on-the-water.jpg',
+    category: 'toy',
+    price: 9,
+    cost: 6.66
+};
+
+const donut = {
+    id: 'pool donut',
+    name: 'pool donut',
+    descriprion: 'Most quintessential pool accessory',
+    image: 'https://miro.medium.com/max/2800/1*CEG1rw2D8mQ3PYDAqVEoAw.jpeg',
+    category: 'floating device',
+    price: 13,
+    cost: 6.66
+
+
+};
+
+const roach = {
+    id: 'giant roach',
+    name: 'giant roach',
+    description: 'Just because you have a copy of The Metamorphosis, doent meant you read it',
+    imafe: 'https://www.kangaroomfg.com/assets/images/products/Products/Cockroach_2.jpg',
+    category: 'floating device',
+    price: 27,
+    cost: 6.66
+};
+
+const ball = {
+    id: 'beach ball',
+    name: 'beach ball',
+    description: 'You cannot use it to float, but you can try',
+    image: 'https://ak5.picdn.net/shutterstock/videos/4752695/thumb/9.jpg',
+    category: 'toy',
+    price: 9,
+    cost: 6.66
+
+};
+
+const raft = {
+    id: 'dog raft',
+    name: 'dog raft',
+    descriprion: 'It is a raft and not a bunch of pool noodles tied together',
+    image: 'https://www.familyhandyman.com/wp-content/uploads/2017/08/fh18djf_583_08_500.jpg',
+    category: 'floating and survival device',
+    price: 99,
+    cost: 6.66
+};
+
+const drinkDonuts = {
+    id: 'drink floats',
+    name: 'drink floats',
+    descriprion: 'drinks, they want to float too',
+    imafe: 'https://lcpshop.net/wp-content/uploads/2019/12/409156-ddqdx0.jpg',
+    category: 'floating',
+    price: 50,
+    cost: 6.99
+
+};
+
+const floaties = [
+    noodle,
+    donut,
+    roach,
+    ball,
+    raft,
+    drinkDonuts
+];
+
+export default floaties;
---
+++ products/render-product.js
@@ -0,0 +1,8 @@
+function renderFloaties(floaties) {
+    const li = docuemnt, createElement('li');
+    li.className = floatie.category;
+    li.title = floatie.description;
+
+
+
+}
