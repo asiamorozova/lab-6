f4afcfe782209dda5187102afea78b12c3d6f9af
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-23 00:32:44 -0800

    resumission

--- data/product.js
+++ data/product.js
@@ -1,7 +1,7 @@
 const noodle = {
     id: 'pool noodle',
     name: 'pool noodle',
-    description: 'Not sure what they are for, but they float';
+    description: 'Not sure what they are for, but they float',
     image: 'https://storage.needpix.com/rsynced_images/pool-noodle-on-the-water.jpg',
     category: 'toy',
     price: 9,
@@ -11,7 +11,7 @@ const noodle = {
 const donut = {
     id: 'pool donut',
     name: 'pool donut',
-    descriprion: 'Most quintessential pool accessory',
+    description: 'Most quintessential pool accessory',
     image: 'https://miro.medium.com/max/2800/1*CEG1rw2D8mQ3PYDAqVEoAw.jpeg',
     category: 'floating device',
     price: 13,
@@ -24,7 +24,7 @@ const roach = {
     id: 'giant roach',
     name: 'giant roach',
     description: 'Just because you have a copy of The Metamorphosis, doent meant you read it',
-    imafe: 'https://www.kangaroomfg.com/assets/images/products/Products/Cockroach_2.jpg',
+    image: 'https://www.kangaroomfg.com/assets/images/products/Products/Cockroach_2.jpg',
     category: 'floating device',
     price: 27,
     cost: 6.66
@@ -44,7 +44,7 @@ const ball = {
 const raft = {
     id: 'dog raft',
     name: 'dog raft',
-    descriprion: 'It is a raft and not a bunch of pool noodles tied together',
+    description: 'It is a raft and not a bunch of pool noodles tied together',
     image: 'https://www.familyhandyman.com/wp-content/uploads/2017/08/fh18djf_583_08_500.jpg',
     category: 'floating and survival device',
     price: 99,
@@ -54,8 +54,8 @@ const raft = {
 const drinkDonuts = {
     id: 'drink floats',
     name: 'drink floats',
-    descriprion: 'drinks, they want to float too',
-    imafe: 'https://lcpshop.net/wp-content/uploads/2019/12/409156-ddqdx0.jpg',
+    description: 'drinks, they want to float too',
+    image: 'https://lcpshop.net/wp-content/uploads/2019/12/409156-ddqdx0.jpg',
     category: 'floating',
     price: 50,
     cost: 6.99
